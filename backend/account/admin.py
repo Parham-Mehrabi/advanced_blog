b69d3117383797e9416a54ab909e5c1dd5a37de8
Parham-Mehrabi <parham.mehrabi.webdev@gmail.com>
2023-05-16 23:18:22 +0330

    start the django project and setup a costum user model and start 3 apps

---
+++ backend/account/admin.py
@@ -0,0 +1,38 @@
+from django.contrib.auth.admin import UserAdmin
+from django.contrib import admin
+from django.contrib.auth import get_user_model
+
+user = get_user_model()
+
+
+class CustomUserAdmin(UserAdmin):
+    model = user
+    list_display = ("email", "is_staff", "is_superuser", "is_active", "is_verified")
+    list_filter = ("email", "is_staff", "is_superuser", "is_active")
+    search_fields = ("email",)
+    ordering = ("email",)
+
+    fieldsets = (
+        ('AUTH', {'fields': ('email', 'password')}),
+        ('STATUS', {'fields': ("is_staff", "is_superuser", "is_active", "is_verified")})
+    )
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "fields": (
+                    "email",
+                    "password1",
+                    "password2",
+                    "is_staff",
+                    "is_superuser",
+                    "is_active",
+                    "is_verified",
+                )
+            },
+        ),
+    )
+
+
+admin.site.register(user, CustomUserAdmin)
---
+++ backend/account/models/user.py
@@ -0,0 +1,60 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.utils.translation import gettext_lazy as _
+
+
+class CustomUserManager(BaseUserManager):
+    """
+        User Manager which handle creating users
+    """
+
+    def create_user(self, email, password, **kwargs):
+        """ handle creating users """
+
+        if not email:
+            raise ValueError(_('this field is required'))
+        email = self.normalize_email(email)
+
+        user = self.model(email=email, **kwargs)
+        user.set_password(password)
+        user.save()
+
+    def create_superuser(self, email, password, **kwargs):
+
+        kwargs.setdefault('is_staff', True)
+        kwargs.setdefault('is_active', True)
+        kwargs.setdefault('is_superuser', True)
+        kwargs.setdefault('is_verified', True)
+
+        if kwargs.get('is_staff') is not True:
+            raise ValueError(_('super user most have is_staff=True'))
+        if kwargs.get('is_active') is not True:
+            raise ValueError(_('super user most have is_active=True'))
+        if kwargs.get('is_superuser') is not True:
+            raise ValueError(_('super user most have is_superuser=True'))
+        if kwargs.get('is_verified') is not True:
+            raise ValueError(_('super user most have is_verified=True'))
+
+        self.create_user(email, password, **kwargs)
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    """
+        custom user model
+    """
+
+    email = models.EmailField(max_length=256, unique=True)
+    is_superuser = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_verified = models.BooleanField(default=True)  # Todo: change default to false and verify using email
+    is_active = models.BooleanField(default=True)
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
