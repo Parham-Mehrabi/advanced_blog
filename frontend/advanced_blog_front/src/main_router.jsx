d73ea6d88994a645c6253e79fbdbd9d5bd6488f2
Parham-Mehrabi <parham.mehrabi.webdev@gmail.com>
2023-05-24 14:07:37 +0330

    start category page

--- frontend/advanced_blog_front/src/main_router.jsx
+++ frontend/advanced_blog_front/src/main_router.jsx
@@ -3,12 +3,14 @@ import React from "react";
 import App from './App.jsx'
 import NotFound from './errors/404.jsx'
 import Home from './components/home.jsx'
+import Categories from './components/category.jsx'
+
 
 const router = createBrowserRouter(
         createRoutesFromElements(
         <Route path={'/'} element={<App/>}>
             <Route index element={<Home/>}/>
-
+            <Route path='/category' element={<Categories/>}/>
             <Route path='*' element={<NotFound/>} />
         </Route>
     )
