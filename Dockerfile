3b20186126a1ef620da66e63325de2843865f352
Parham-Mehrabi <parham.mehrabi.webdev@gmail.com>
2023-06-03 19:28:29 +0330

    change data base to postgres

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,7 @@ ENV PYTHONUNBUFFERED=1
 
 WORKDIR /app
 COPY ./backend /app/
-
+RUN apt update
+RUN apt install -y python3-dev libpq-dev build-essential
 RUN pip3 install --upgrade pip
 RUN pip3 install -r ./requirements.txt
--- backend/core/settings.py
+++ backend/core/settings.py
@@ -88,8 +88,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django',
+        'HOST': 'psql',
+        'PORT': '5432',
+        'USER': 'parham',
+        'PASSWORD': os.getenv('PARHAM_PASSWORD'),
     }
 }
 
