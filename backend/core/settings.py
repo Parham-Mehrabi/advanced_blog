134a2506d16b3c2ba2cc29b15be5eda173707e5e
Parham-Mehrabi <parham.mehrabi.webdev@gmail.com>
2023-06-13 06:36:01 +0330

    typo in ALLOWED HOSTS

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = 'django-insecure-8cj^$(+fay(r!18g!^*p@c0vc@-f!4q9q^jwilkqlq=n9rbdn6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://blog.parham-webdev.com']
+ALLOWED_HOSTS = ['blog.parham-webdev.com']
 
 # TODO: change it to my domain
 
