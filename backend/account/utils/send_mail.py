bc0f1a9374d1603830326fecb513fa63ad2fdd30
Parham-Mehrabi <parham.mehrabi.webdev@gmail.com>
2023-06-13 16:46:20 +0330

    fix the SMTP variables wasnt avalible in worker container

--- backend/account/utils/send_mail.py
+++ backend/account/utils/send_mail.py
@@ -9,39 +9,39 @@ def send_password_reset_token(email, user, token):
     send {token} to {user}'s {email}
     """
     if settings.DEBUG:
-        email = EmailMessage(
+        email_obj = EmailMessage(
             "email/password_reset.tpl",
             {"token": token},
             "parham-webdev@parham.com",
             to=[email],
         )
-        email.send()
+        email_obj.send()
     else:
-        email = EmailMessage(
+        email_obj = EmailMessage(
             "email/password_reset.tpl",
             {"token": token,
              "user": user},
-            settings.EMAIL_HOST_USER,
+            'mail.parham-webdev.com',
             to=[email],
         )
-        email.send()
+        email_obj.send()
 
 
 @shared_task
 def send_email_verification(email, token):
     if settings.DEBUG:
-        email = EmailMessage(
+        email_obj = EmailMessage(
             "email/verify_email.tpl",
             {"token": token},
             "parham-webdev@parham.com",
             to=[email],
         )
-        email.send()
+        email_obj.send()
     else:
-        email = EmailMessage(
+        email_obj = EmailMessage(
             "email/verify_email.tpl",
             {"token": token},
-            settings.EMAIL_HOST_USER,
+            'mail.parham-webdev.com',
             to=[email],
         )
-        email.send()
+        email_obj.send()
