8af6d0eac5d8d0be54b9e6269e2b59d851fe3466
Parham-Mehrabi <parham.mehrabi.webdev@gmail.com>
2023-06-13 06:01:44 +0330

    fix the bug where github CI use's docker-compose file instead if docker-compose-deploy one

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -156,7 +156,7 @@ REST_FRAMEWORK = {
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://bredis:6379/1",
+        "LOCATION": "redis://redis:6379/1",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
@@ -164,7 +164,7 @@ CACHES = {
 }
 
 # celery:
-CELERY_BROKER_URL = 'redis://bredis:6379/2'
+CELERY_BROKER_URL = 'redis://redis:6379/2'
 
 CELERY_BEAT_SCHEDULE = {
     'delete_completed_tasks': {
